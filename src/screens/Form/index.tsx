706a793be7767820343e762ed966a85306febd96
Michel John <michel.santos@dataside.com.br>
2022-04-22 17:35:21 -0300

    add auto captalize words

--- src/screens/Form/index.tsx
+++ src/screens/Form/index.tsx
@@ -9,6 +9,16 @@ export function Form() {
   const { navigate } = useNavigation();
 
   const [loading, setLoading] = useState(false);
+  
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  function resetForm() {
+    setName('');
+    setEmail('');
+    setMessage('');
+  }
 
   function handleSubmitForm() {
     setLoading(true);
@@ -17,6 +27,8 @@ export function Form() {
       setLoading(false);
       
       navigate('Home');
+
+      resetForm();
     }, 2000);
   }
 
@@ -24,24 +36,32 @@ export function Form() {
     <S.View>
       <S.Logo />
 
-      <S.Input placeholder='Your name' editable={!loading} />
       <S.Input
+        value={name}
+        editable={!loading}
+        autoCapitalize='words'
+        onChangeText={setName}
+        placeholder='Your name' />
+      <S.Input
+        value={email}
         editable={!loading}
         autoCapitalize='none'
+        onChangeText={setEmail}
         keyboardType='email-address'
         placeholder='Your best e-mail' />
 
       <S.InputMessage
         multiline
+        value={message}
         editable={!loading}
         textAlignVertical='top'
+        onChangeText={setMessage}
         placeholder='Type your message here' />
 
       <Button title='Submit'
         enabled={!loading}
         isLoading={loading}
         onPress={handleSubmitForm} />
-        
     </S.View>
   );
 };
